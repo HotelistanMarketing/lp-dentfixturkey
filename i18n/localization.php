92bedfa97b9ef78ac106546e01f33337a2fc5434
Gündüz <3319717+gndzkrkc@users.noreply.github.com>
2023-08-02 11:26:33 +0300

    Add content

---
+++ i18n/localization.php
@@ -0,0 +1,119 @@
+<?php
+
+$TR = array(
+    "page_title" => "Turkey Dental Clinic in Istanbul | DentfixTurkey",
+    "page_keywords" => "dental clinic, turkey, veneers, crowns, hollywood smile, restorative dentistry",
+    "page_description" => "Our expert team of dentists offers a wide range of services for a beautiful and healthy smile. Schedule your appointment today and experience exceptional dental care at Turkey Dental Teeth Clinic - DentfixTurkey.",
+
+    "whatsapp_main_keyword" => "dental implant",
+    "whatsapp_message" => "Hello, I would like to know about the services and prices offered for %s in Turkey.",
+
+    // HEADER
+    "nav_menu" => array(
+        "why" => "Why Choose",
+        "ba" => "Before & After",
+        "influencers" => "Influencers",
+        "services" => "Services",
+        "team" => "Our Team",
+        "footer" => "Contact"
+    ),
+
+    // HERO
+    "hero_title" => "Dental Clinic in <br><span>Turkey, Istanbul</span>",
+    "hero_subtitle" => "Enhance Your Smile<br>with Dentfix Turkey’s Affordable Services",
+    "hero_button" => "Get the Best Dental Solution",
+    "hero_org_title" => "Our Reliable Brands",
+
+    // FORM
+    "form_title" => "Reshape Your Beautiful Smile with Dentfix",
+    "form_description" => "Fill out the form, and let our experts quickly respond to any question marks in your mind.",
+    "form_button" => "Hurry Up! Get A Free Quote",
+    "form_name" => "Name & Surname",
+    "form_mail" => "Mail",
+    "form_phone" => "Phone",
+    "form_empty_field_warning" => "%s cannot be empty.",
+    "form_phone_validation" => "Please fill a valid phone number.",
+
+    // WHY CHOOSE
+    "why_title" => "Why Choose Us?",
+    "why" => array(
+        "Dentfix offers comprehensive dental treatment services <strong>with state-of-the-art technology</strong> and personalized care.",
+        "<strong>All In One</strong> Services",
+        "<strong>+15780</strong> Happy Patients",
+        "<strong>+17 Years</strong> of Experience",
+        "<strong>Cutting-Edge Technology</strong>",
+        "<strong>Affordable</strong> Prices"
+    ),
+
+    // SERVICES
+    "services_title" => "Check Out Our Highly Recommended Dental Solutions",
+    "service_card_button" => "Get A Price",
+    "services_details" => array(
+        ["Dental Implants",
+            "The Mini Brazilian Butt Lift is a specialized procedure that involves the removal and transfer of fat from two liposuction zones to achieve a more subtle enhancement of the buttocks."],
+        ["Dental Veneers",
+            "The Plus Size Brazilian Butt Lift is a type of cosmetic surgery that utilizes the patient's own body fat to enhance the size and shape of their buttocks. It is intended for those with a body mass index ranging from 30 to 35."],
+        ["Dental Crowns",
+            "Non-surgical Brazilian Butt Lift typically involves the injections of a silicone-like material. It can be achieved through non-invasive or minimally invasive techniques such as injectable dermal fillers or radio frequency treatments."],
+        ["Hollywood Smile",
+            "Hourglass BBL is a specific type of Brazilian Butt Lift that aim to enhance the curves of the body. Fat transfers to the buttocks, creating a more defined waistline and a fuller, curvier buttock shape, resulting in a more hourglass-like figure."],
+        ["Restorative Dentistry",
+            "Hourglass BBL is a specific type of Brazilian Butt Lift that aim to enhance the curves of the body. Fat transfers to the buttocks, creating a more defined waistline and a fuller, curvier buttock shape, resulting in a more hourglass-like figure."],
+    ),
+
+    // BEFORE AND AFTER
+    "ba_title" => "Before & After",
+    "ba_button" => "Get the Best Dental Treatment",
+
+    // TEAM
+    "team_title" => "Our Team",
+
+    // CALL TO ACTION
+    "cta_title" => "Free Online Consultation",
+    "cta_subtitle" => "Virtual Consultation:",
+    "cta_description" => "Connect with us before your surgery for a personalized pre-op meeting",
+    "cta_button" => "Contact Us",
+
+    // CLINIC
+    "hospital_title" => "Discover Our Clinic with Photos",
+
+    // PACKAGE
+    "package_title" => "Dentfix Service Details Content",
+    "package_button" => "Get Price Information",
+    "package_details" => array(
+        ["All In One Packages",
+            "Discover Dentfix’s full range of dental services, including restorations, procedures, and solutions designed to suit all of your dental requirements."],
+        ["Schedule A Consultation",
+            "Get in touch with us today to explore the treatment options tailored to help you attain a vibrant and stunning smile that radiates both health and beauty."],
+        ["VIP Airport Transfer",
+            "Arriving in Istanbul?  Leave the logistics to us. Our dedicated team will take care of your airport pickup and local transportation needs, offering a fleet of luxurious VIP minivans for your comfort and convenience."],
+        ["Accommodation",
+            "Experience exclusivity at our centrally located hotels in Istanbul and our partnered first-class establishments. Discover unparalleled opportunities for a memorable stay."],
+        ["Sightseeing Tour",
+            "Uncover the captivating beauty of Turkey, a truly remarkable destination. Maximize your experience with our guided tours and translation services, immersing yourself in unforgettable moments."],
+        ["Post-Op Care",
+            "Preserving the long-term results of your procedure is our priority. Benefit from our post-operative care and support, enabling you to establish enduring healthy habits."],
+    ),
+
+    // INFLUENCERS
+    "influencers_title" => "Youtube Influencer Videos",
+
+    // FAQ
+    "faq_title" => "Frequently Asked Questions",
+    "faq" => array(
+        ["Is implant very painful?",
+            "Lorem ipsum dolor sit amet."],
+        ["How long does Hollywood smile last?",
+            "Lorem ipsum dolor sit amet."],
+        ["How long does dental veneer last?",
+            "Dental veneers can endure for a long period, particularly if they are well adhered to the enamel. While composite veneers normally have a lifespan of 7–10 years, strong porcelain veneers have the ability to survive for 20 years or even longer. "],
+        ["What does a dental crown do?",
+            "Lorem ipsum dolor sit amet."],
+        ["Is it painful to get a crown?",
+            "Lorem ipsum dolor sit amet."],
+    ),
+
+    // FOOTER
+    "footer_social_title" => "Our Social Media",
+    "footer_copyright" => sprintf("©Allrights Dentfix %s", date('Y')),
+);
