49f4f6eb1d6e34952c7a4c2420bbba0a934535f5
Gündüz <3319717+gndzkrkc@users.noreply.github.com>
2023-08-12 17:16:33 +0300

    Add form validation

--- templates/body-end.php
+++ templates/body-end.php
@@ -9,6 +9,6 @@
 </script>
 
 <script src="/scripts/shared/form-country-input.js" defer></script>
-<!--<script src="scripts/shared/form-field-check.js" defer></script>-->
 <script src="/scripts/shared/wp-link-trigger.js" defer></script>
+<?php include get_script('/shared/form-validation.php'); ?>
 <script src="/scripts/shared/faq-accordion.js" defer></script>
