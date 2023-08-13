7677a4c4ac83ec1066b78fcd31acdb43fdc1ec4a
Gündüz <3319717+gndzkrkc@users.noreply.github.com>
2023-08-13 14:23:42 +0300

    Optimize scripts and decrease JQuery usage.

--- templates/body-end.php
+++ templates/body-end.php
@@ -1,15 +1,13 @@
 <script src="/scripts/main.js" defer></script>
-<!--<script src="scripts/shared/int-tel-input.min.js"></script>-->
+<script src="/scripts/shared/wp-link-trigger.js" defer></script>
 <script src="https://cdn.jsdelivr.net/npm/intl-tel-input@18.1.1/build/js/intlTelInput.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
+<script src="/scripts/shared/form-country-input.js" defer></script>
+<?php include get_script('/shared/form-validation.php'); ?>
 
 <?php /* TODO switch to vanilla JS if possible! */ ?>
 <script src="https://code.jquery.com/jquery-3.7.0.min.js"
         integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g="
         crossorigin="anonymous">
 </script>
-
-<script src="/scripts/shared/form-country-input.js" defer></script>
-<script src="/scripts/shared/wp-link-trigger.js" defer></script>
-<?php include get_script('/shared/form-validation.php'); ?>
 <script src="/scripts/shared/faq-accordion.js" defer></script>
