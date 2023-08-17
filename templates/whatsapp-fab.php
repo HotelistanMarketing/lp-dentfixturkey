80ac371a3c5067d4150bd77eb79519ffbdc65d32
Gündüz <3319717+gndzkrkc@users.noreply.github.com>
2023-08-17 13:27:11 +0300

    Improve WhatsApp fab animation

--- templates/whatsapp-fab.php
+++ templates/whatsapp-fab.php
@@ -10,8 +10,8 @@
 <a id="whatsapp-fab" href="<?= WHATSAPP_LINK ?>" target="_blank">
     <img class="wp-icon" src="/assets/ico-wp.png" width="130" height="91" alt="WhatsApp">
     <div class="wp-dots">
-        <div class="circle1 circle"></div>
-        <div class="circle2 circle"></div>
-        <div class="circle3 circle"></div>
+        <div class="dot one"></div>
+        <div class="dot two"></div>
+        <div class="dot three"></div>
     </div>
 </a>
