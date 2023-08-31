47f57c269a723b6aa0345f0615ab1b24b43cc83c
Gündüz <3319717+gndzkrkc@users.noreply.github.com>
2023-08-31 16:20:36 +0300

    Add Turkish translations

---
+++ i18n/implant-tr.php
@@ -0,0 +1,146 @@
+<?php
+
+const TR = array(
+    "page_title" => "Türkiye Diş Kliniği İstanbul | Dentfix Türkiye",
+    "page_keywords" => "diş kliniği, türkiye, porselen laminalar, kaplamalar, hollywood gülüşü, restoratif diş hekimliği",
+    "page_description" => "Uzman Diş Hekimlerimiz, güzel ve sağlıklı bir gülümseme için geniş bir hizmet yelpazesi sunuyor. Randevunuzu bugün ayarlayın ve DentfixTürkiye - Türkiye Diş Tedavi Kliniği'nde olağanüstü diş bakımını deneyimleyin.",
+
+    "whatsapp_main_keyword" => "diş implantı",
+    "whatsapp_message" => "Merhaba, Türkiye'de %s için sunulan hizmetler ve fiyatlar hakkında bilgi almak istiyorum.",
+
+    // HEADER
+    "nav_menu" => array(
+        "why" => "Neden Biz",
+        "before-after" => "Öncesi & Sonrası",
+        "influencers" => "Influencerlar",
+        "services" => "Hizmetler",
+        "team" => "Ekibimiz",
+        "footer" => "İletişim"
+    ),
+
+    // HERO
+    "hero_title" => "Türkiye, İstanbul'da <br><span>Diş Kliniği</span>",
+    "hero_subtitle" => "Dentfix Türkiye'nin Uygun Fiyatlı Hizmetleriyle<br>Gülümsemenizi Güzelleştirin",
+    "hero_button" => "En İyi Diş Tedavisine Ulaşın",
+    "hero_org_title" => "Güvenilir Markalarımız",
+
+    // FORM
+    "form_title" => "Güzel Gülüşünüzü Dentfix ile Yeniden Şekillendirin",
+    "form_description" => "Uzmanlarımızın aklınıza takılan bütün soruları hızlıca cevaplaması için formu doldurun.",
+    "form_button" => "Acele Edin! Ücretsiz Fiyat Teklifi Alın",
+    "form_name" => "Adınız & Soyadınız",
+    "form_mail" => "E-posta Adresiniz",
+    "form_phone" => "Telefon Numaranız",
+    "form_empty_field_warning" => "%s boş olamaz.",
+    "form_phone_validation" => "Lütfen geçerli bir telefon numarası giriniz.",
+
+    // WHY CHOOSE
+    "why_title" => "Neden Bizi Tercih Etmelisiniz?",
+    "why" => array(
+        "<strong>Gelişmiş Teknoloji</strong>",
+        "<strong>Hepsi Bir Arada</strong> Hizmetlerimiz",
+        "<strong>+15780</strong> Mutlu Hastalar",
+        "<strong>+17 Yıllık</strong> Deneyim",
+        "<strong>Son Teknoloji</strong>",
+        "<strong>Uygun</strong> Fiyatlar"
+    ),
+    "why_descriptions" => array(
+        "En son teknolojiyle gelişmiş tıbbi bakımı deneyimleyin.",
+        "Hepsi bir arada pratik ve kapsamlı sağlık hizmetleri.",
+        "Bize güvenen binlerce memnun hastadan biri olun.",
+        "17 yılı aşkın deneyimli uzmanlıktan yararlanın.",
+        "Kesin tanı ve tedavi için son teknoloji araçlar.",
+        "Bütçe dostu fiyatlarla kaliteli bakım."
+    ),
+
+    // SERVICES
+    "services_title" => "Tavsiye edilen Diş Çözümlerimizi inceleyin",
+    "service_card_button" => "Fiyat Alın",
+    "services_details" => array(
+        ["Diş İmplantları",
+            "Diş İmplantı Paketlerimiz:",
+            "All-On-4",
+            "All-On-6",
+            "All-On-8",
+            "3-On-6",
+            "Tam Ağız Diş İmplantı"],
+        ["Diş Kaplamaları",
+            "Diş Kaplamaları Paketlerimiz:",
+            "Kompozit Kaplamalar",
+            "Porselen Kaplamalar",
+            "E-max Kaplamalar",
+            "Lamina Kaplamalar",
+            "Aşındırmasız Kaplamalar",
+            "Tam Kaplama Seti",
+            "Tam Ağız Diş İmplantı"],
+        ["Diş Kronları",
+            "Diş Kronları Paketlerimiz:",
+            "Celtra Kronları",
+            "E-max Kronları",
+            "Zirkonyum Kronları",
+            "Porselen Kronları",
+            "Tam Kron Seti"],
+        ["Hollywood Gülüşü",
+            "Gelişmiş tekniklerimiz ve uzmanlığımızla dönüştürücü bir gülümseme estetiğini deneyimleyin.<br>Baştan sona sağlığınızı güvence altına alan Türkiye'de Hollywood Gülüşü ile iyileşme sürecinizi sorunsuz ve rahat bir şekilde geçirin."],
+        ["Restoratif Diş Hekimliği",
+            "Diş implantları, kaplamalar, köprüler veya diğer restoratif işlemlere ihtiyacınız olsun ya da olmasın, doğal sonuçlar üreten tam çözümler sunuyoruz."],
+    ),
+
+    // BEFORE AND AFTER
+    "ba_title" => "Öncesi & Sonrası",
+    "ba_button" => "En İyi Diş Tedavisine Ulaşın",
+
+    // TEAM
+    "team_title" => "Ekibimiz",
+
+    // CALL TO ACTION
+    "cta_title" => "Ücretsiz Çevrimiçi Danışmanlık",
+    "cta_subtitle" => "Sanal Danışmanlık:",
+    "cta_description" => "Ameliyat öncesi kişiselleştirilmiş bir ön görüşme için bizimle iletişime geçin",
+    "cta_button" => "Bize Ulaşın",
+
+    // CLINIC
+    "hospital_title" => "Kliniğimizi Fotoğraflarla Keşfedin",
+
+    // PACKAGE
+    "package_title" => "Dentfix Hizmet Detayları İçeriği",
+    "package_button" => "Ücret Bilgisi Alın",
+    "package_details" => array(
+        ["Hepsi Bir Arada Paketleri",
+            "Dentfix'in kapsamlı diş hizmetlerini keşfedin; restorasyonlar, prosedürler ve tüm ihtiyaçlarınıza uygun çözümler içeren hizmetlerimizi öğrenin."],
+        ["Danışmanlık Randevusu Ayarlayın",
+            "Bizimle bugün iletişime geçin ve size özel bir şekilde tasarlanmış tedavi seçeneklerini keşfedin. Bu seçenekler sayesinde, sağlık ve güzelliği yansıtan canlı ve çarpıcı bir gülümseme elde etmenize yardımcı olacağız."],
+        ["VIP Havaalanı Transferi",
+            "İstanbul'a mı geliyorsunuz? Lojistik işlerinizi bize bırakın. Özel ekibimiz, havaalanı transferi ve yerel ulaşım ihtiyaçlarınızı karşılamak için lüks VIP minivanlar sunarak rahatınız ve konforunuz için sizinle ilgilenecekler."],
+        ["Konaklama",
+            "İstanbul'daki merkezi konumlu otellerimizde ve anlaşmalı olduğumuz birinci sınıf tesislerimizde özel bir deneyim yaşayın. Unutulmaz bir konaklama için eşsiz fırsatları keşfedin."],
+        ["Şehir Turu",
+            "Gerçekten olağanüstü bir destinasyon olan Türkiye'nin büyüleyici güzelliğini keşfedin. Rehberli turlarımız ve çeviri hizmetlerimizle deneyiminizi en üst düzeye çıkarın, unutulmaz anlar yaşayın."],
+        ["Ameliyat Sonrası Bakım",
+            "İşleminizin uzun vadeli sonuçlarını korumak önceliğimizdir. Kalıcı sağlıklı alışkanlıklar edinmenizi sağlayacak ameliyat sonrası bakım ve desteğimizden yararlanın."],
+    ),
+
+    // INFLUENCERS
+    "influencers_title" => "Youtube Influencer Videoları",
+
+    // FAQ
+    "faq_title" => "Sıkça Sorulan Sorular",
+    "faq" => array(
+        ["İmplant çok acı verir mi?",
+            "Diş implantı işlemi sırasında implant alanının etrafındaki sinirleri uyuşturmak için lokal anestezi kullanılacak ve bu herhangi bir acı hissetmenizi engelleyecektir. Biraz baskı yaşayabilirsiniz ama bu rahatsız edici olmayacak."],
+        ["Hollywood gülümsemesi ne kadar süre dayanır?",
+            "Hollywood gülümsemesinin süresi, kullanılan diş implantlarının, kronların veya kaplamaların dayanıklılığına göre belirlenir. Porselen kaplamaların ömrü 20 yıldan fazla, kompozit kaplamaların ömrü ise yaklaşık 10 yıldır."],
+        ["Diş kaplama ne kadar dayanır?",
+            "Diş kaplamaları, özellikle mineye iyi yapıştıkları takdirde uzun süre dayanabilirler. Kompozit kaplamaların normalde 7-10 yıllık bir ömrü varken, güçlü porselen kaplamaların 20 yıl ve hatta daha uzun süre dayanabilme özelliği vardır."],
+        ["Diş kronu ne işe yarar?",
+            "Diş kronları, çürümüş, hasar görmüş, zayıflamış veya yıpranmış bir dişi restore etmek için kullanılabilen, dişe benzeyen bir başlıktır. Ayrıca diş hekimleri kök tedavisi görmüş dişleri ve diş implantlarını kronlarla kaplayabilir. Kronlar beş ila on beş yıl arasında dayanabilir."],
+        ["İmplant çok acı verir mi?",
+            "Diş implantı işlemi sırasında implant alanının etrafındaki sinirleri uyuşturmak için lokal anestezi kullanılacak ve bu herhangi bir acı hissetmenizi engelleyecektir. Biraz baskı yaşayabilirsiniz ama bu rahatsız edici olmayacak."],
+        ["Kron işlemi acı verici mi?",
+            "Kron işlemi için yapılan düzenli diş tedavisi, vereceği rahatsızlık hissinden korktukları için bazı kişileri tedirgin edebilir. Ancak diş hekiminiz tedaviye başlamadan önce ağzınızı uyuşturacağından işlem rahatsızlık verici olmayacak. Bu, ilk randevudan son işleme kadar prosedürün her adımı için geçerlidir."],
+    ),
+
+    // FOOTER
+    "footer_social_title" => "Sosyal Medya Hesaplarımız",
+    "footer_copyright" => "Tüm hakları saklıdır © %s - Dentfix",
+);
---
+++ public/implant-tr/config.php
@@ -0,0 +1,9 @@
+<?php
+
+const VARIANT = 'implant';
+const LANG_CODE = 'tr';
+const LEAD_LANGUAGE = 'Turkish';
+const LEAD_TREATMENTS = 'Dental';
+const LEAD_TREATMENT = 'Implant';
+const LEAD_SOURCE = 'Google/Dentfix/Tr/Implant';
+const LEAD_DESCRIPTION = 'Dentfix Türkçe LP';
