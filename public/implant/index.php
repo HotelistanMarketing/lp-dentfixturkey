b5550ed120954e0a5cacb28242ef173e6f0327df
Gündüz <3319717+gndzkrkc@users.noreply.github.com>
2023-09-07 22:30:20 +0300

    Performance score tests

--- public/implant/index.php
+++ public/implant/index.php
@@ -3,8 +3,6 @@ include 'config.php';
 include $_SERVER['DOCUMENT_ROOT'] . '/i18n/implant-' . LANG_CODE . '.php';
 include $_SERVER['DOCUMENT_ROOT'] . '/functions.php';
 include $_SERVER['DOCUMENT_ROOT'] . '/data.php';
-
-header('Expires: ' . gmdate('D, d M Y H:i:s \G\M\T', time() + (60 * 60))); // 1 hour
 ?>
 
 <!DOCTYPE HTML>
