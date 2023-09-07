e6e680c9163bce9d6323e542e079f69a50864bef
Gündüz <3319717+gndzkrkc@users.noreply.github.com>
2023-09-07 20:01:50 +0300

    Commonize WhatsApp FAB

--- public/implant/index.php
+++ public/implant/index.php
@@ -26,7 +26,7 @@ include $_SERVER['DOCUMENT_ROOT'] . '/data.php';
     <?php include get_template('faq.php') ?>
 </main>
 <?php include get_template('footer.php') ?>
-<?php include get_template('whatsapp-fab.php') ?>
+<?php include get_template('whatsapp-fab.php', common: true) ?>
 <?php include get_template('body-end.php') ?>
 </body>
 </html>
