7fa5560b5567834f3cfeaa9d727a6f77cc930dfb
Gündüz <3319717+gndzkrkc@users.noreply.github.com>
2023-09-08 18:18:05 +0300

    Test French page

---
+++ i18n/implant-fr.php
@@ -0,0 +1,150 @@
+<?php
+
+const TR = array(
+    "page_title" => "Clinique dentaire à Istanbul | DentfixTurquie",
+    "page_keywords" => "clinique dentaire, dinde, facettes, couronnes, sourire hollywoodien, dentisterie réparatrice",
+    "page_description" => "Notre équipe d'experts dentistes offre une large gamme de services pour un sourire beau et sain. Prenez rendez-vous dès aujourd'hui et profitez d'une prise en charge dentaire exceptionnelle à la clinique dentaire DentfixTurkey en Turquie.",
+
+    "whatsapp_main_keyword" => "implant dentaire",
+    "whatsapp_message" => "Bonjour, je voudrais savoir sur les services et les prix offerts pour %s en Turquie.",
+
+    // HEADER
+    "nav_menu" => array(
+        "why" => "Pourquoi Nous Choisir ",
+        "before-after" => "Avant & Après",
+        "influencers" => "Influenceurs",
+        "services" => "Services",
+        "team" => "Notre équipe",
+        "footer" => "Contacter"
+    ),
+
+    // BANNER
+    "banner_header" => "<span>L'Automne vous appelle!..</span> Offre Spéciale. Économisez jusqu'a 25% et plus.",
+
+    // HERO
+    "hero_title" => "Clinique dentaire en Turquie",
+    "hero_title_mobile" => "Clinique Dentaire en Turquie",
+    "hero_subtitle" => "Sublimez votre sourire<br> avec les services abordables de Dentfix Turquie",
+    "hero_button" => "Obtenez la meilleure solution dentaire",
+    "hero_org_title" => "Nos marques de qualité",
+
+    // FORM
+    "form_title" => "Rendez votre beau sourire plus radieux avec Dentfix",
+    "form_description" => "Remplissez le formulaire et permettez à nos experts de répondre rapidement à toutes vos questions.",
+    "form_button" => "Ne tardez pas ! Obtenez un devis gratuit dès aujourd'hui !",
+    "form_name" => "Nom et Prénom",
+    "form_mail" => "Courrier",
+    "form_phone" => "Téléphone",
+    "form_empty_field_warning" => "%s ne peut pas être vide.",
+    "form_phone_validation" => "Veuillez fournir un numéro de téléphone valide.",
+
+    // WHY CHOOSE
+    "why_title" => "Pourquoi Nous Choisir?",
+    "why" => array(
+        "<strong>La meilleure Technologie</strong>",
+        "<strong>All In One</strong> Services",
+        "<strong>+15780</strong> Patients heureux",
+        "<strong>+17 ans</strong> d'expérience",
+        "<strong>La Dernière des Technologie</strong>",
+        "Prix <strong>abordables</strong>"
+    ),
+    "why_descriptions" => array(
+        "Faites confiance aux dernières technologies pour vous offrir les meilleurs soins médicaux possibles.",
+        "Des soins de santé de qualité, accessibles et sans tracas.",
+        "Rejoignez des milliers de patients satisfaits qui nous font confiance.",
+        "Bénéficiez de plus de 17 ans d'expertise spécialisée.",
+        "Des outils de pointe pour un diagnostic et un traitement précis.",
+        "Des soins de qualité à des tarifs avantageux."
+    ),
+
+    // SERVICES
+    "services_title" => "Consultez nos solutions dentaires fortement recommandées",
+    "service_card_button" => "Obtenir un prix",
+    "services_details" => array(
+        ["Implants dentaires",
+            "Nos Packages d'Implants Dentaires:",
+            "All On Four",
+            "All On Six",
+            "All On Eight",
+            "Three On Six",
+            "Implant dentaire pour toute la bouche"],
+        ["Facettes Dentaire",
+            "Nos Packages de Facettes dentaire :",
+            "Facette composites",
+            "Facettes en porcelaine",
+            "Facettes E-max",
+            "Facette Laminée",
+            "facettes sans préparation",
+            "Ensemble complet de Facette",
+            "Implant dentaire pour toute la bouche"],
+        ["Couronnes dentaires",
+            "Nos Packages de Facettes dentaire :",
+            "Couronnes Celtra",
+            "Couronnes E-max",
+            "Couronnes de zirconium",
+            "Couronnes en porcelaine",
+            "Ensemble complet de Couronnes"],
+        ["Hollywood Smile",
+            "Faites l'expérience d'un relooking de sourire transformateur avec nos techniques et expertises avancées.<br>Profitez d'un processus de récupération du Hollywood Smile en Turquie fluide et confortable, en assurant votre bien-être."],
+        ["Dentisterie réparatrice",
+            "Peu importe si vous avez besoin d'implants dentaires, de couronnes, de ponts ou d'autres procédures de restauration, nous offrons des solutions complètes qui produisent des résultats qui semblent naturels."],
+    ),
+
+    // BEFORE AND AFTER
+    "ba_title" => "Avant & Après",
+    "ba_button" => "Obtenez le meilleur traitement dentaire",
+
+    // TEAM
+    "team_title" => "Notre équipe",
+
+    // CALL TO ACTION
+    "cta_title" => "Consultation en ligne gratuite",
+    "cta_subtitle" => "Consultation virtuelle :",
+    "cta_description" => "Prenez contact avec nous avant votre intervention chirurgicale pour une consultation préopératoire personnalisée.",
+    "cta_button" => "Contactez-nous",
+
+    // CLINIC
+    "hospital_title" => "Découvrez notre Clinique avec Photos",
+
+    // PACKAGE
+    "package_title" => "Informations sur les services Dentfix",
+    "package_button" => "Obtenir des informations sur le prix",
+    "package_details" => array(
+        ["Tous en un seul Package",
+            "Découvrez la gamme complète des services dentaires de Dentfix, y compris les restaurations, les procédures et les solutions conçues pour répondre à tous vos besoins dentaires."],
+        ["Planifier une consultation",
+            "Prenez contact avec nous dès aujourd'hui pour explorer les options de traitement adaptées à vos besoins et vous aider à obtenir un sourire vibrant et magnifique qui rayonne à la fois santé et beauté."],
+        ["Transfert VIP de l'aéroport",
+            "Arrivez à Istanbul ? Notre équipe dédiée s'occupera de votre prise en charge à l'aéroport et de vos besoins en matière de transport local, en vous proposant une flotte de minivans VIP luxueux pour votre confort et votre commodité."],
+        ["Hébergement",
+            "Découvrez l'exclusivité de nos hôtels situés dans le centre d'Istanbul et de nos établissements partenaires de première classe."],
+        ["Visite guidée",
+            "Découvrez la beauté captivante de la Turquie, une destination vraiment remarquable. Maximisez votre expérience grâce à nos visites guidées et nos services de traduction, en vous immergeant dans des moments inoubliables."],
+        ["Soins Post-Op",
+            "Préserver les résultats à long terme de votre procédure est notre priorité. Profitez de nos soins postopératoires et de notre soutien, vous permettant ainsi d'établir des habitudes saines et durables."],
+    ),
+
+    // INFLUENCERS
+    "influencers_title" => "Vidéos d'influenceur Youtube",
+
+    // FAQ
+    "faq_title" => "Foire aux questions",
+    "faq" => array(
+        ["La marque de l'implant est-elle importante ? L'implant a-t-il de la qualité ?",
+            "Pour nos procédures dentaires, Dentfix Turquie n'utilise que des produits premium tels que Ivoclar Vivadent, Straumann et Nobel Biocare Implant. Ces produits sont réputés pour leur excellence, garantissant des résultats durables et bénéfiques pour nos patients. Notre engagement à fournir les meilleurs soins dentaires est reflété par notre engagement envers ces grandes marques. Pour plus d'informations, vous pouvez consulter le Journal européen de dentisterie, une source réputée de recherche dentaire."],
+        ["Combien de temps dure le Hollywood Smile ?",
+            "La durée du Hollywood Smile est déterminée par la durabilité des implants, des couronnes ou des facettes utilisés. Les facettes en porcelaine peuvent durer plus de 20 ans, tandis que facettes composites durent environ 10 ans."],
+        ["Combien de temps dure la facette dentaire?",
+            "Les facettes dentaires peuvent durer longtemps, surtout si elles sont bien adhérentes. Les facettes composites ont généralement une durée de vie de 7 à 10 ans, tandis que les facettes en porcelaine solide peuvent durer 20 ans ou plus."],
+        ["Que fait une couronne dentaire?",
+            "Une couronne dentée est un bouchon qui ressemble à une dent qui peut être utilisée pour restaurer une dent qui a été décomposée, endommagé, affaibli ou usé. De plus, les professionnels dentaires peuvent couvrir les dents traitées à la racine et les implants dentaires avec des couronnes, qui peuvent durer entre cinq et quinze ans."],
+        ["L'implant est-il très douloureux?",
+            "Pendant la procédure d'implant dentaire, une anesthésie locale sera utilisée pour engourdir les nerfs autour de la zone de l'implant. Cela vous empêchera de ressentir de la douleur. Vous pouvez ressentir une certaine pression, mais ce n'est pas inconfortable."],
+        ["Est-il douloureux d'obtenir une couronne?",
+            "La procédure dentaire habituelle de pose d'une couronne peut rendre certaines personnes nerveuses car elles ont peur de l'inconfort. Cependant, comme votre dentiste anesthésiera votre bouche avant de commencer tout traitement, la procédure ne devrait pas être inconfortable. Ceci est valable pour chaque étape de la procédure, du premier rendez-vous au dernier essayage."],
+    ),
+
+    // FOOTER
+    "footer_social_title" => "Nos réseaux sociaux",
+    "footer_copyright" => "Tous droits réservés ©️ %s - Dentfix",
+);
---
+++ public/implant-fr/config.php
@@ -0,0 +1,8 @@
+<?php
+
+const VARIANT = 'implant';
+const LANG_CODE = 'fr';
+const LEAD_LANGUAGE = 'French';
+const LEAD_INTEREST = 'Dental';
+const LEAD_PROCEDURE = 'Implant';
+const LEAD_SOURCE = 'Dentfix/Google/FR/Implant';
