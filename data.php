535c2a646226cc8d7f5fb4e6e48360c27c5558d1
Gündüz <3319717+gndzkrkc@users.noreply.github.com>
2023-09-09 15:25:46 +0300

    Correct Zoho form name

--- data.php
+++ data.php
@@ -10,7 +10,7 @@ const INSTAGRAM_LINK = 'https://www.instagram.com/dentfix.turkey/';
 const TIKTOK_LINK = 'https://www.tiktok.com/@dentfixturkey';
 
 const GTM_ID = 'GTM-W4KM7W2';
-const FORM_NAME = 'WebToLeads4841144000105221001';
+const FORM_NAME = 'WebToLeads4841144000107484302';
 const FORM_THANK_YOU_PAGE = 'https://dentfixturkey.com/thank-you';
 const FORM_xmIwtLD = '0e65f475011faf7c6ff4c6942b525aed4359637541a529b149b48e7c3a350e75';
 const FORM_ANAL_RID = '0e65f475011faf7c6ff4c6942b525aed4359637541a529b149b48e7c3a350e75gidf26b68fc575ddfcaee437b8e12d5515126eae05cf5d1a5976d576056a6237774gid885e3c1045bd9bdcc91bdf30f82b5696gid14f4ec16431e0686150daa43f3210513&tw=61690b96c1d0471b638f31426f38e68aa67fb7ed6da86f32dc10ad817fe55a0a';
