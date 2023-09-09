cba101410b47494e4811d3e797110517cb632cc0
Gündüz <3319717+gndzkrkc@users.noreply.github.com>
2023-09-09 13:06:46 +0300

    Revert "Performance test with deferred scripts"
    This reverts commit dac926751f43b8aa0770d294e2dc1a42fb85e6db.

--- templates/body-end.php
+++ templates/body-end.php
@@ -1,14 +1,14 @@
 <script src="/scripts/main.js" defer></script>
 <script src="/commons/scripts/modernizr-webp.js" defer></script>
 <script src="/commons/scripts/wp-link-trigger.js" defer></script>
-<script src="https://cdn.jsdelivr.net/npm/intl-tel-input@18.1.1/build/js/intlTelInput.min.js" defer></script>
-<script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js" defer></script>
+<script src="https://cdn.jsdelivr.net/npm/intl-tel-input@18.1.1/build/js/intlTelInput.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
 <script src="/commons/scripts/form-country-input.js" defer></script>
 <?php include get_script('form-validation.php', common: true); ?>
 
 <?php /* TODO switch to vanilla JS if possible! */ ?>
 <script src="https://code.jquery.com/jquery-3.7.0.min.js"
         integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g="
-        crossorigin="anonymous" async>
+        crossorigin="anonymous">
 </script>
 <script src="/commons/scripts/faq-accordion.js" defer></script>
