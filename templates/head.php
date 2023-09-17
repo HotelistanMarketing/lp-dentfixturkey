5377d6fcca1e3ff6e0ca28af8979b2bafe8cbe1d
Gündüz <3319717+gndzkrkc@users.noreply.github.com>
2023-09-17 11:41:26 +0300

    Boost FCP with inline styling and preloading

--- templates/head.php
+++ templates/head.php
@@ -4,12 +4,12 @@
     <?php get_google_font('Roboto', '300;400;700') ?>
     <link rel="preload" as="image" media="(max-width: 600px)" href="<?= get_webp('/assets/hero-mobile.jpg') ?>">
     <link rel="preload" as="image" media="(min-width: 600.1px)" imagesrcset="<?= get_srcset('/assets/hero.jpg') ?>">
-    <link rel="preload" as="style" href="/public/implant/critic.css">
-    <link rel="stylesheet" href="/public/implant/critic.css">
+    <link rel="preload" as="style" href="/public/implant/style.css">
     <link rel="stylesheet" href="/public/implant/style.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/intl-tel-input@18.1.1/build/css/intlTelInput.min.css">
+    <?php get_preload_style('https://cdn.jsdelivr.net/npm/intl-tel-input@18.1.1/build/css/intlTelInput.min.css'); ?>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css"/>
     <link rel="icon" type="image/x-icon" href="/assets/ico-favicon.svg">
+    <style><?php include($_SERVER['DOCUMENT_ROOT'] . '/public/implant/critic.css') ?></style>
 
     <?php include get_script('gtm.php', common: true) ?>
 </head>
