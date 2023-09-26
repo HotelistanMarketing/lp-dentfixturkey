55592b50f14412bcae01af77a1cd860242dca50d
Gündüz <3319717+gndzkrkc@users.noreply.github.com>
2023-09-26 11:22:52 +0300

    Move sections as requested

--- public/implant/index.php
+++ public/implant/index.php
@@ -19,12 +19,12 @@ include $_SERVER['DOCUMENT_ROOT'] . '/data.php';
     <?php endif ?>
     <?php include get_template('hero.php') ?>
     <?php include get_template('why.php') ?>
-    <?php include get_template('services.php') ?>
     <?php include get_template('before-after.php') ?>
+    <?php include get_template('package.php') ?>
+    <?php include get_template('services.php') ?>
     <?php include get_template('team.php') ?>
     <?php include get_template('cta.php') ?>
     <?php include get_template('hospital.php') ?>
-    <?php include get_template('package.php') ?>
     <?php include get_template('cta.php') ?>
     <?php include get_template('influencers.php') ?>
     <?php include get_template('faq.php') ?>
