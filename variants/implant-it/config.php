4bba8b66ff14248ada667d42cc0187e65e280419
Gündüz <3319717+gndzkrkc@users.noreply.github.com>
2023-11-04 18:41:21 +0300

    Improve styling

---
+++ variants/implant-it/config.php
@@ -0,0 +1,8 @@
+<?php
+
+const VARIANT = 'implant';
+const LANG_CODE = 'tr';
+const LEAD_LANGUAGE = 'Turkish';
+const LEAD_INTEREST = 'Dental';
+const LEAD_PROCEDURE = 'Implant';
+const LEAD_SOURCE = 'Google/Dentfix/Tr/Implant';
