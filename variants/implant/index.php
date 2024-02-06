abfbd8cb5d557f8771465ec78663c4088c47a670
mozanyazar <mahmutozanyazar@gmail.com>
2024-02-06 14:50:06 +0300

    sticky info removed

--- variants/implant/index.php
+++ variants/implant/index.php
@@ -13,24 +13,25 @@ include $_SERVER['DOCUMENT_ROOT'] . '/data.php';
 <!DOCTYPE HTML>
 <html lang="<?= LANG_CODE ?>">
 <?php include get_template('head.php') ?>
+
 <body class="preload">
-<main>
-    <?php include get_template('banner.php') ?>
-    <?php include get_template('hero.php') ?>
-    <?php include get_template('why.php') ?>
-    <?php include get_template('before-after.php') ?>
-    <?php include get_template('package.php') ?>
-    <?php include get_template('services.php') ?>
-    <?php include get_template('team.php') ?>
-    <?php include get_template('cta.php') ?>
-    <?php include get_template('hospital.php') ?>
-    <?php include get_template('cta.php') ?>
-    <?php include get_template('influencers.php') ?>
-    <?php include get_template('faq.php') ?>
-</main>
-<?php include get_template('footer.php') ?>
-<?php include get_template('whatsapp-fab.php', common: true) ?>
-<?php include get_template('wp-form.php', common: true) ?>
-<?php include get_template('body-end.php') ?>
+    <main>
+        <?php include get_template('hero.php') ?>
+        <?php include get_template('why.php') ?>
+        <?php include get_template('before-after.php') ?>
+        <?php include get_template('package.php') ?>
+        <?php include get_template('services.php') ?>
+        <?php include get_template('team.php') ?>
+        <?php include get_template('cta.php') ?>
+        <?php include get_template('hospital.php') ?>
+        <?php include get_template('cta.php') ?>
+        <?php include get_template('influencers.php') ?>
+        <?php include get_template('faq.php') ?>
+    </main>
+    <?php include get_template('footer.php') ?>
+    <?php include get_template('whatsapp-fab.php', common: true) ?>
+    <?php include get_template('wp-form.php', common: true) ?>
+    <?php include get_template('body-end.php') ?>
 </body>
+
 </html>
