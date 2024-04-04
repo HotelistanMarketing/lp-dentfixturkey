83c0613db897f7f0e8c6d8db80671c8a8aff2e0e
Gündüz <3319717+gndzkrkc@users.noreply.github.com>
2024-04-04 23:59:20 +0300

    Fix Gulp script concatenation order

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,52 @@
 const gulp = require('gulp');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
+const order = require('gulp-order');
+
+const mainJSFiles = [
+    'commons/scripts/intl-tel-input.min.js',
+    'commons/scripts/wp-fab.js',
+    'commons/scripts/wp-link-trigger.js',
+    'commons/scripts/fix-transition-glitch.js',
+    'commons/scripts/form-country-input.js',
+    'commons/scripts/form-validation.js',
+    'commons/scripts/faq-accordion.js',
+    'commons/scripts/wp-form.js',
+];
+
+const criticalJSFiles = [
+    'commons/scripts/swiper-bundle.min.js',
+    'scripts/swiper.js',
+    'commons/scripts/optimize.js',
+];
+
+const fbJSFiles = [
+    'commons/scripts/intl-tel-input.min.js',
+    'commons/scripts/form-country-input.js',
+    'commons/scripts/form-validation.js',
+    'commons/scripts/wp-link-trigger.js',
+    'commons/scripts/fix-transition-glitch.js',
+];
 
 function concat_and_uglify_js() {
-    const mainJob = gulp.src([
-        'commons/scripts/intl-tel-input.min.js',
-        'commons/scripts/wp-fab.js',
-        'commons/scripts/wp-link-trigger.js',
-        'commons/scripts/fix-transition-glitch.js',
-        'commons/scripts/form-country-input.js',
-        'commons/scripts/form-validation.js',
-        'commons/scripts/faq-accordion.js',
-        'commons/scripts/wp-form.js',
-    ])
+    const mainJob = gulp.src(mainJSFiles)
+        .pipe(order(mainJSFiles, { base: './' }))
         .pipe(concat('main.min.js'))
         .pipe(uglify({
             'mangle': {toplevel: false},  // Keep intention clear - true causes bugs!
         }))
-        .pipe(gulp.dest('scripts'))
+        .pipe(gulp.dest('scripts'));
 
-    const criticalJs = gulp.src([
-        'commons/scripts/swiper-bundle.min.js',
-        'scripts/swiper.js',
-        'commons/scripts/optimize.js',
-    ])
+    const criticalJs = gulp.src(criticalJSFiles)
+        .pipe(order(criticalJSFiles, { base: './' }))
         .pipe(concat('critical.min.js'))
         .pipe(uglify({
             'mangle': {toplevel: true},
         }))
         .pipe(gulp.dest('scripts'));
 
-    const fbJob = gulp.src([
-        'commons/scripts/intl-tel-input.min.js',
-        'commons/scripts/form-country-input.js',
-        'commons/scripts/form-validation.js',
-        'commons/scripts/wp-link-trigger.js',
-        'commons/scripts/fix-transition-glitch.js',
-    ])
+    const fbJob = gulp.src(fbJSFiles)
+        .pipe(order(fbJSFiles, { base: './' }))
         .pipe(concat('fb.min.js'))
         .pipe(uglify({
             'mangle': {toplevel: false},  // Keep intention clear - true causes bugs!
