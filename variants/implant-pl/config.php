05746198ac966b8f3c9c01b9cd67d60471309052
Mahmut Ozan Yazar <101927776+mozanyazar@users.noreply.github.com>
2024-05-05 12:04:49 +0300

    Update config.php

--- variants/implant-pl/config.php
+++ variants/implant-pl/config.php
@@ -2,5 +2,5 @@
 
 const VARIANT = 'implant';
 const LANG_CODE = 'pl';
-const LEAD_LANGUAGE = "Lead\Language\Polish";
+const LEAD_LANGUAGE = "Polish";
 const LEAD_SOURCE = 'Dentfix/Google/Pl/Implant';
