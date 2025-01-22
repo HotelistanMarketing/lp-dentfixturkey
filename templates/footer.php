7f92f58f69a4dbe728fe07c9457941f2a31056ba
mozanyazar <mahmutozanyazar@gmail.com>
2025-01-22 17:46:01 +0300

    update footer logo

--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,6 @@
 <footer id="footer">
     <div class="container">
-        <img class="logo" src="/assets/ico-logo-white.svg" width="206" height="52" alt="logo" loading="lazy"/>
+        <img class="logo" src="/assets/dentfix-logo-1.svg" width="206" height="52" alt="logo" loading="lazy" />
         <div class="footer-info">
             <div>
                 <img class="icon" src="/assets/ico-location.svg" width="20" height="20" alt="address" loading="lazy">
@@ -36,7 +36,7 @@
         <div class="container">
             <nav>
                 <?php foreach (TR['nav_menu'] as $key => $value): ?>
-                    <a href="#<?= $key ?>"><?= $value ?></a>
+                <a href="#<?= $key ?>"><?= $value ?></a>
                 <?php endforeach; ?>
             </nav>
             <span class="copyright-text"><?= sprintf(TR['footer_copyright'], date('Y')) ?></span>
