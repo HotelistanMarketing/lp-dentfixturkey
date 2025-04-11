3db2b0c1438f3498b21e5837295d7a1e93368a60
mozanyazar <mahmutozanyazar@gmail.com>
2025-04-11 13:09:54 +0300

    fix

--- templates/header.php
+++ templates/header.php
@@ -1,6 +1,6 @@
 <header>
     <div class="container">
-        <?php if ($_SERVER['HTTP_HOST'] === 'dentaltreatmentsturkey'): ?>
+        <?php if (str_contains($_SERVER['HTTP_HOST'], 'dentaltreatmentsturkey.com')): ?>
         <img src="/assets/logo-2.svg" width="153" height="39" alt="logo" />
         <?php else: ?>
         <img src="/assets/dentfix-logo-1.svg" width="153" height="39" alt="logo" />
