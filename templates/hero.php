61ca8ea279d6ab1314782053a8e282049d871400
mozanyazar <mahmutozanyazar@gmail.com>
2025-04-11 14:39:50 +0300

    change hero

--- templates/hero.php
+++ templates/hero.php
@@ -1,29 +1,65 @@
-<section id="hero" class="parallax">
+<section id="hero">
     <?php include get_template('header.php') ?>
-
     <div class="container">
-        <div class="hero-content">
+        <div class="hero-grid">
+            <img src="/assets/ba-1.jpeg" loading="eager">
+            <img src="/assets/ba-2.jpeg" loading="eager">
+            <img src="/assets/ba-3.jpeg" loading="eager">
+            <img src="/assets/ba-5.jpeg" loading="eager">
+            <img src="/assets/ba-6.jpeg" loading="eager">
+            <img src="/assets/ba-7.jpeg" loading="eager">
+            <img src="/assets/ba-8.jpeg" loading="eager">
+            <img src="/assets/ba-9.jpeg" loading="eager">
+            <img src="/assets/ba-10.jpeg" loading="eager">
+            <img src="/assets/ba-11.jpeg" loading="eager">
+            <img src="/assets/ba-12.jpeg" loading="eager">
+            <img src="/assets/ba-5.jpeg" loading="eager">
+            <img src="/assets/ba-6.jpeg" loading="eager">
+            <img src="/assets/ba-7.jpeg" loading="eager">
+            <img src="/assets/ba-6.jpeg" loading="eager">
+            <img src="/assets/ba-6.jpeg" loading="eager">
+        </div>
+        <div id="hero-content" class="hero-content">
             <h1><?= TR["hero_title"] ?></h1>
             <span class="hero-subtitle"><?= TR["hero_subtitle"] ?></span>
-            <a class="button" href="#why"><?= TR['hero_button'] ?></a>
+            <?php get_wp_button(TR['hero_button']) ?>
+            <a href="#contact" class="ico-arrow">
+                <img src="/assets/ico-arrow.svg" width="29" height="34" alt="" loading="eager">
+            </a>
         </div>
-        <div id="org-section">
+    </div>
+
+    <div id="org-section" class="section">
+        <div class="container">
+            <?php // TODO optimize these! ?>
             <picture>
-                <source media="(min-width:992px)" srcset="/assets/ico-logo-nobel-lg.svg" width="126" height="41">
-                <img src="/assets/ico-logo-nobel.svg" width="76" height="25" alt="Nobel Biocare" loading="lazy">
+                <source media="(min-width:992px)" srcset="/assets/logo-iso-desktop.png" width="194" height="69">
+                <img src="/assets/logo-iso.png" width="138" height="47" alt="ISO">
             </picture>
             <picture>
-                <source media="(min-width:992px)" srcset="/assets/ico-logo-straumann-lg.svg" width="146" height="19">
-                <img src="/assets/ico-logo-straumann.svg" width="90" height="12" alt="Straumann" loading="lazy">
+                <source media="(min-width:992px)" srcset="/assets/logo-tcsb-desktop.png" width="79" height="79">
+                <img src="/assets/logo-tcsb.png" width="56" height="57" alt="Ministry of Health">
             </picture>
             <picture>
-                <source media="(min-width:992px)" srcset="/assets/ico-logo-ejd-lg.svg" width="130" height="47">
-                <img src="/assets/ico-logo-ejd.svg" width="84" height="31" alt="EJD" loading="lazy">
-            </picture>
-            <picture>
-                <source media="(min-width:992px)" srcset="/assets/ico-logo-ivoclar-lg.svg" width="130" height="47">
-                <img src="/assets/ico-logo-ivoclar.svg" width="84" height="31" alt="Ivoclar Vivadent" loading="lazy">
+                <source media="(min-width:992px)" srcset="/assets/logo-jci-desktop.png" width="202" height="110">
+                <img src="/assets/logo-jci.png" width="144" height="41" alt="Joint Commission International">
             </picture>
         </div>
     </div>
+
+    <script>
+    const media_query_desktop = window.matchMedia("(min-width: 992px)")
+    desktopMQLE(media_query_desktop)
+    media_query_desktop.onchange = desktopMQLE
+
+    function desktopMQLE(event) {
+        // Update organization images' parent!
+        const org_section = document.getElementById("org-section")
+
+        if (event.matches)
+            document.getElementById("hero-content").appendChild(org_section)
+        else
+            document.getElementById("hero").appendChild(org_section)
+    }
+    </script>
 </section>
