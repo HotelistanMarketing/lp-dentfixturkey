5ca8fed89d6e13b3846d9308b78ec25dfed34af9
Utku Baygüven <79966502+utkubayguven@users.noreply.github.com>
2025-10-11 13:00:36 +0300

    Integrate Yandex.Metrika for dental implant page
    Added Yandex.Metrika tracking script for specific URL.

--- templates/head.php
+++ templates/head.php
@@ -45,4 +45,20 @@
     <?php include get_script('gtm.php', common: true) ?>
     <?php include get_script('hotjar.php', common: true) ?>
     <?php include get_script('logs.php', common: true) ?>
+    
+    <?php if (str_contains($_SERVER['HTTP_HOST'], 'dentfixinternational.com') && $_SERVER['REQUEST_URI'] === '/dental-implant-in-turkey-ru/'): ?>
+    <!-- Yandex.Metrika counterr -->
+    <script type="text/javascript">
+        (function(m,e,t,r,i,k,a){
+            m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+        })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=104584738', 'ym');
+
+        ym(104584738, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+    </script>
+    <noscript><div><img src="https://mc.yandex.ru/watch/104584738" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <!-- /Yandex.Metrika counter -->
+    <?php endif; ?>
 </head>
