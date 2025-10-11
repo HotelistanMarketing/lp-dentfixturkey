0f05d24be3492194cabd4460d4b2661be0ab4813
Utku Bayguven <utkubayguven@distedavim.com>
2025-10-11 17:09:31 +0300

    update: add WhatsApp contact number for Russian implant variant

--- variants/implant-ru/config.php
+++ variants/implant-ru/config.php
@@ -4,3 +4,6 @@ const VARIANT = 'implant';
 const LANG_CODE = 'ru';
 const LEAD_LANGUAGE = "Lead\Language\Russian";
 const LEAD_SOURCE = 'Dentfix/Google/RU/Implant';
+
+// Rusça sayfa için özel WhatsApp numarası
+define('READABLE_PHONE_NUMBER', '905494593841');
