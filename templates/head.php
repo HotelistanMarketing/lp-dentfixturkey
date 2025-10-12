863485d92154e2c1b297531b97f0176aceea1a40
ensdmr15 <enes.d@novavitaclinica.com>
2025-10-12 19:25:09 +0300

    Update head.php

--- templates/head.php
+++ templates/head.php
@@ -1,4 +1,14 @@
 <head>
+    <?php if (str_contains($_SERVER['HTTP_HOST'], 'dentfixdentalclinic.com')): ?>
+    <!-- Google Tag Manager for dentfixdentalclinic.com -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-K73HN7FZ');</script>
+    <!-- End Google Tag Manager -->
+    <?php endif; ?>
+
     <?php include get_template('meta.php', common: true) ?>
 
     <link rel="preload" as="image" media="(max-width: 600px)" href="<?= get_webp('/assets/hero-mobile.jpg') ?>">
