a18ef4577dcec754e3e7c6dee712c8d17a4c7718
Utku Bayguven <utkubayguven@distedavim.com>
2025-10-13 11:18:38 +0300

    Add Spanish localization for dental services and remove Polish variant files

--- pages/implant-dentystyczny-turcjia/config.php
+++
@@ -1,2 +0,0 @@
-<?php
-include $_SERVER['DOCUMENT_ROOT'] . '/variants/implant-pl/config.php';
---
+++ variants/implant-es/config.php
@@ -0,0 +1,6 @@
+<?php
+
+const VARIANT = 'implant-es';
+const LANG_CODE = 'es';
+const LEAD_LANGUAGE = Lead\Language\Spanish;
+const LEAD_SOURCE = 'Google/Dentfix/Es/Implant';
