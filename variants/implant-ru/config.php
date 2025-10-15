d52d0f97e2c6f7a2a194cc217067293c82cfdc72
Utku Bayguven <utkubayguven@distedavim.com>
2025-10-15 16:40:52 +0300

    Update Russian variant configuration for lead sources

--- variants/implant-ru/config.php
+++ variants/implant-ru/config.php
@@ -3,7 +3,10 @@
 const VARIANT = 'implant';
 const LANG_CODE = 'ru';
 const LEAD_LANGUAGE = "Lead\Language\Russian";
-const LEAD_SOURCE = 'Dentfix/Google/RU/Implant';
+const LEAD_SOURCE = 'google/webform';
+const LEAD_SOURCE_DETAIL = 'Yandex Ads';
+const LEAD_SOURCE_API = 'Pick_List_7';
+const LEAD_SOURCE_DETAIL_API = 'Single_Line_5';
 
 // Rusça sayfa için özel WhatsApp numarası
 define('READABLE_PHONE_NUMBER', '905494593841');
