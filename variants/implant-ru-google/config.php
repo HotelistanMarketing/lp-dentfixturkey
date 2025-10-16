474af8092d5785cb524b4ba7b7bee65afaf723fd
Utku Bayguven <utkubayguven@distedavim.com>
2025-10-16 23:03:59 +0300

    Fix Telegram link comment typo in Russian implant variant configuration

--- variants/implant-ru-google/config.php
+++ variants/implant-ru-google/config.php
@@ -6,7 +6,7 @@ const LEAD_LANGUAGE = "Lead\Language\Russian";
 const LEAD_SOURCE = 'Google/Dentfix/Ru/Implant';
 const LEAD_SOURCE_DETAIL = 'Google Ads';  // Google Ads tracking
 
-// Telegram için direkt link
+// Telegram için direkt linkS
 const TELEGRAM_LINK_RU = 'https://t.me/+905518622525';
 
 
